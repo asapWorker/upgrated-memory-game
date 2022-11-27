1ab06f6966d1fec07ead423654a8ebb7aebae2ca
asapWorker <m1904976@edu.misis.ru>
2022-11-27 20:47:17 +0300

    add disabling of menu buttons after click

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,5 +1,5 @@
 import './Menu.css';
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import {useDispatch} from "react-redux";
 import {startGame} from "../../store/gameSlice";
 import {EASY, MIDDLE, HARD} from "../../constants";
@@ -8,12 +8,19 @@ import {setRemoteControllerConfig} from "../../functions";
 export function Menu(props) {
   const dispatch = useDispatch();
 
+  const btnEasy = useRef();
+  const btnMiddle = useRef();
+  const btnHard = useRef();
+
   useEffect(() => {
     // set remote controller actions
     setRemoteControllerConfig();
   }, [])
 
   function startGameBtnClickHandler(level) {
+    // disable menu buttons
+    btnEasy.current.disabled = btnMiddle.current.disabled = btnHard.current.disabled = true;
+
     dispatch(startGame(level));
     props.changeAppScreen();
   }
@@ -21,9 +28,9 @@ export function Menu(props) {
 
   return (
     <div className="menu">
-      <button className="btn menu-btn managing-btn" onClick={() => startGameBtnClickHandler(EASY)}>Легкий уровень</button>
-      <button className="btn menu-btn managing-btn" onClick={() => startGameBtnClickHandler(MIDDLE)}>Средний уровень</button>
-      <button className="btn menu-btn managing-btn" onClick={() => startGameBtnClickHandler(HARD)}>Сложный уровень</button>
+      <button ref={btnEasy} className="btn menu-btn managing-btn" onClick={() => startGameBtnClickHandler(EASY)}>Легкий уровень</button>
+      <button ref={btnMiddle} className="btn menu-btn managing-btn" onClick={() => startGameBtnClickHandler(MIDDLE)}>Средний уровень</button>
+      <button ref={btnHard} className="btn menu-btn managing-btn" onClick={() => startGameBtnClickHandler(HARD)}>Сложный уровень</button>
     </div>
   )
 }
